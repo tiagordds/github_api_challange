480d6096e5f008d2a9c7e031dbe5f560ddbc3a87
Tiago Rodrigues <yutri.trds@gmail.com>
2025-02-27 09:50:00 -0300

    uses HTPP requests to get users data

--- github_scraper/batch_fetcher.py
+++ github_scraper/batch_fetcher.py
@@ -2,13 +2,21 @@ import json
 
 import requests
 
-url_users = "https://api.github.com/users"
 
-response = requests.get(url_users)
+def get_users_by_id_define_per_page(since_id=0, per_page=30):
+    url_users = f"https://api.github.com/users?since={since_id}&per_page={per_page}"
+    return url_users
 
-users_info = response.json()
 
-print(f"{len(users_info)}")
+response = requests.get(get_users_by_id_define_per_page())
 
-with open("github_users.json", "w") as file:
-    json.dump(users_info, file, indent=1)
+
+if response.status_code == 200:
+    users_info = response.json()
+    if len(users_info) == 0:
+        raise Exception("No users found, please, try again.")
+    with open("github_users.json", "w") as file:
+        json.dump(users_info, file, indent=1)
+    print(f"Got {len(users_info)} users")
+else:
+    raise Exception(f"Response failed, got HTTP code: {response.status_code}")
