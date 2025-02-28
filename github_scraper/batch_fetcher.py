eee748f59f48fd020bb7de98b0a456f375e15870
Tiago Rodrigues <yutri.trds@gmail.com>
2025-02-28 09:12:44 -0300

    now fetches several users at once

--- github_scraper/batch_fetcher.py
+++ github_scraper/batch_fetcher.py
@@ -1,22 +1,35 @@
 import json
+import time
 
 import requests
+from access_token import PERSONAL_ACCESS_TOKEN
+
+TOKEN = PERSONAL_ACCESS_TOKEN
+
+autorization = {"Autorization": f"token {TOKEN}"}
 
 
-def get_users_by_id_define_per_page(since_id=0, per_page=30):
-    url_users = f"https://api.github.com/users?since={since_id}&per_page={per_page}"
-    return url_users
+def get_users(per_page=100, max_users=1000):
+    users = []
+    since_id = 0
+
+    while len(users) < max_users:
+        url = "https://api.github.com/users"
+        since_per_page = {"since": since_id, "per_page": per_page}
+
+        response = requests.get(url, headers=autorization, params=since_per_page)
+
+        if response.status_code == 200:
+            batch_users = response.json()
+
+        users.extend(batch_users)
+        since_id = batch_users[-1]["id"]
+
+        time.sleep(0.200)
+
+        with open("github_users.json", "w") as file:
+            json.dump(users, file, indent=1)
+    print(len(users))
 
 
-response = requests.get(get_users_by_id_define_per_page())
-
-
-if response.status_code == 200:
-    users_info = response.json()
-    if len(users_info) == 0:
-        raise Exception("No users found, please, try again.")
-    with open("github_users.json", "w") as file:
-        json.dump(users_info, file, indent=1)
-    print(f"Got {len(users_info)} users")
-else:
-    raise Exception(f"Response failed, got HTTP code: {response.status_code}")
+get_users()
