9a4d809d4e4d415f7a9b19eee88b59e173789d9b
Tiago Rodrigues <yutri.trds@gmail.com>
2025-04-22 22:24:37 -0300

    Adds a simple pagination

--- github_scraper/app/views.py
+++ github_scraper/app/views.py
@@ -1,13 +1,24 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import UserInfo
 
 
 def user_list(request):
-    users = UserInfo.objects.all()
-    return render(request, "app/users_list.html", {"users": users})
+    user_list = UserInfo.objects.all().order_by("id")
+    paginator = Paginator(user_list, 50)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "app/users_list.html", {"page_obj": page_obj})
 
 
 def full_user_list(request):
-    users = UserInfo.objects.all()
-    return render(request, "app/full_user_list.html", {"users": users})
+    full_user_list = UserInfo.objects.all().order_by("id")
+    paginator = Paginator(full_user_list, 50)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "app/full_user_list.html", {"page_obj": page_obj})
